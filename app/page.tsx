9d2ad575c3bef7c44e917369288aa0f2d583ef7d
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-08-30 16:07:40 +0200

    chore: setup convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 'use client'
+import { useQuery } from 'convex/react'
 import { useEffect, useState } from 'react'
+import { api } from '../convex/_generated/api'
 
 export default function Home() {
-  const [message, setMessage] = useState()
+  // const [message, setMessage] = useState()
+  const tasks = useQuery(api.tasks.get)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/hello')
-      const { message } = await res.json()
-      setMessage(message)
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('/api/hello')
+  //     const { message } = await res.json()
+  //     setMessage(message)
+  //   }
+  //   fetchData()
+  // }, [])
 
-  if (!message) return <p>Loading...</p>
+  // if (!message)
+  //   return <p>Loading...</p>
 
-  return <p>{message}</p>
+  return (
+    <div>
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    </div>
+  )
 }
