3e7deeb874658fb1429408ee4f7673d0b49b594a
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-09-10 16:44:13 +0200

    dashboard correct suspense and layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,27 @@
 import { Suspense } from 'react'
+import DashboardHeading from '@/components/dashboard/DashboardHeading'
 import DashboardView from '@/components/dashboard/DashboardView'
+import CreateProjectDialog from '@/components/project/CreateProjectDialog'
 
 export default function Dashboard() {
+  const breadcrumbs = [
+    {
+      label: 'home',
+      url: '/',
+    },
+  ]
+
   return (
-    <>
+    <div className="md:px-6 pb-2 md:pt-6 flex flex-col h-screen">
+      <DashboardHeading
+        title="Dashboard"
+        actions={<CreateProjectDialog>New Project</CreateProjectDialog>}
+        breadcrumbs={breadcrumbs}
+      />
       <Suspense fallback={<div>loading</div>}>
         <DashboardView />
       </Suspense>
-    </>
+    </div>
+
   )
 }
--- components/dashboard/DashboardView.tsx
+++ components/dashboard/DashboardView.tsx
@@ -1,32 +1,16 @@
 'use client'
 import { useQuery } from 'convex/react'
-import CreateProjectDialog from '@/components/project/CreateProjectDialog'
 import { api } from '@/convex/_generated/api'
-import DashboardHeading from './DashboardHeading'
 import DashboardProjectCard from './DashboardProjectCard'
 
 export default function DashboardView() {
   const projects = useQuery(api.projects.getProjectsForUser)
 
-  const breadcrumbs = [
-    {
-      label: 'home',
-      url: '/',
-    },
-  ]
-
   return (
-    <div className="md:px-6 pb-2 md:pt-6 flex flex-col h-screen">
-      <DashboardHeading
-        title="Dashboard"
-        actions={<CreateProjectDialog>New Project</CreateProjectDialog>}
-        breadcrumbs={breadcrumbs}
-      />
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 max-md:px-2 overflow-y-scroll">
-        {projects && projects.map(project =>
-          <DashboardProjectCard project={project} key={project._id} />,
-        )}
-      </div>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 max-md:px-2 overflow-y-scroll">
+      {projects && projects.map(project =>
+        <DashboardProjectCard project={project} key={project._id} />,
+      )}
     </div>
   )
 }
