e1293cae9173d7712f267fa105945de5110a46ca
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-09-11 16:44:46 +0200

    chore: removed wrapper

--- components/Status/StatusForwardBadge.tsx
+++ components/Status/StatusForwardBadge.tsx
@@ -10,31 +10,38 @@ export default function StatusForwardBadge({ status }: StatusForwardBadgeProps) 
     return
 
   return (
-    <>
-      <div className="flex text-xs items-center gap-2 group text-secondary-foreground/50 hover:text-secondary-foreground transition-colors cursor-pointer">
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            {status.state.current.displayName}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
+    <div className="flex text-xs items-center gap-2 group text-secondary-foreground/50 hover:text-secondary-foreground transition-colors cursor-pointer">
+      <DropdownMenu>
 
-            <DropdownMenuLabel>Change status to</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuRadioGroup value={status.state.current._id} onValueChange={val => status.methods.setById(val as Id<'requestStatuses'>)}>
-              {status.state.statuses?.map(stat => (
-                <DropdownMenuRadioItem key={stat._id} value={stat._id}>{stat.displayName}</DropdownMenuRadioItem>
-              ))}
-            </DropdownMenuRadioGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <DropdownMenuTrigger>
+          {status.state.current.displayName}
+        </DropdownMenuTrigger>
 
-        <button
-          onClick={status.methods.setNext}
-          className="-translate-x-4 opacity-0 group-hover:translate-0 group-hover:opacity-100 transition-all hover:bg-secondary rounded-sm p-1 cursor-pointer"
-        >
-          <ChevronRight size={16} />
-        </button>
-      </div>
-    </>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Change status to</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={status.state.current._id}
+            onValueChange={val => status.methods.setById(val as Id<'requestStatuses'>)}
+          >
+            {status.state.statuses?.map(stat => (
+              <DropdownMenuRadioItem
+                key={stat._id}
+                value={stat._id}
+              >
+                {stat.displayName}
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <button
+        onClick={status.methods.setNext}
+        className="-translate-x-4 opacity-0 group-hover:translate-0 group-hover:opacity-100 transition-all hover:bg-secondary rounded-sm p-1 cursor-pointer"
+      >
+        <ChevronRight size={16} />
+      </button>
+    </div>
   )
 }
