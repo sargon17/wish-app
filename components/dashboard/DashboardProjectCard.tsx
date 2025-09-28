46a7ecba256bb1b4ff33d12ac2358c8286a8c01b
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-09-28 18:22:28 +0200

    copy project id functionality

--- components/dashboard/DashboardProjectCard.tsx
+++ components/dashboard/DashboardProjectCard.tsx
@@ -1,11 +1,26 @@
 import type { Doc } from '@/convex/_generated/dataModel'
+import { text } from 'node:stream/consumers'
+import { Copy } from 'lucide-react'
 import Link from 'next/link'
+import { toast } from 'sonner'
 import { sluggedText } from '@/lib/slug'
+import { copyToClipboard } from '@/lib/text'
+import { Button } from '../ui/button'
 import { Card, CardAction, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import DashboardProjectCardActions from './DashboardProjectCardActions'
 
 interface Props {
   project: Doc<'projects'>
+}
+
+interface HandleCopyClickProps extends Pick<Parameters<typeof copyToClipboard>[0], 'text'> {}
+
+function handleCopyClick({ text }: HandleCopyClickProps) {
+  copyToClipboard({
+    text,
+    onSuccess: () => toast.info('Project ID copied successfully'),
+    onFail: () => toast.error('Error during Project ID copy'),
+  })
 }
 
 export default function DashboardProjectCard({ project }: Props) {
@@ -17,7 +32,13 @@ export default function DashboardProjectCard({ project }: Props) {
       <Link href={`dashboard/project/${project._id}/${sluggedText(project.title)}`} className="absolute inset-0 z-0" />
       <CardHeader>
         <CardTitle className=" capitalize">{project.title}</CardTitle>
-        <CardDescription className="overflow-hidden text-ellipsis " onClick={e => e.stopPropagation()}>{project._id}</CardDescription>
+        <div className="overflow-hidden flex gap-2 items-center">
+          <CardDescription className="overflow-hidden text-ellipsis" onClick={e => e.stopPropagation()}>{project._id}</CardDescription>
+          <Button variant="ghost" size="icon" className="relative z-1" onClick={() => handleCopyClick({ text: project._id })}>
+            <Copy size={12} />
+          </Button>
+
+        </div>
         <CardAction>
           <DashboardProjectCardActions id={project._id} />
         </CardAction>
--- lib/text.ts
+++ lib/text.ts
@@ -9,3 +9,15 @@ export function trimTo({ text, to = 100 }: TrimToProps) {
 
   return `${text.slice(0, to)}...`
 }
+
+interface CopyToClipboardProps {
+  text: string
+  onSuccess?: () => void
+  onFail?: () => void
+}
+
+export function copyToClipboard({ text, onSuccess, onFail }: CopyToClipboardProps) {
+  navigator.clipboard.writeText(text)
+    .then(() => onSuccess && onSuccess())
+    .catch(() => onFail && onFail())
+}
