7fe5071318f08b4170180d8c9559562934b222a6
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-10-05 17:00:43 +0200

    scroll area

--- components/dashboard/DashboardView.tsx
+++ components/dashboard/DashboardView.tsx
@@ -1,16 +1,19 @@
 'use client'
 import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
+import { ScrollArea } from '../ui/scroll-area'
 import DashboardProjectCard from './DashboardProjectCard'
 
 export default function DashboardView() {
   const projects = useQuery(api.projects.getProjectsForUser)
 
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 max-md:px-2 overflow-y-scroll">
-      {projects && projects.map(project =>
-        <DashboardProjectCard project={project} key={project._id} />,
-      )}
-    </div>
+    <ScrollArea>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 max-md:px-2">
+        {projects && projects.map(project =>
+          <DashboardProjectCard project={project} key={project._id} />,
+        )}
+      </div>
+    </ScrollArea>
   )
 }
