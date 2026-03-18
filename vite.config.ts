8bcd25219312cdae551164add179d41b6dee49d2
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2026-03-18 21:35:19 +0100

    chore: some setup things

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite-plus'
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
   run: {
@@ -6,11 +6,48 @@ export default defineConfig({
       scripts: true,
       tasks: true,
     },
+    tasks: {
+    },
   },
   lint: {
+    plugins: ["node", "jsdoc", "import", "unicorn", "react"],
+    ignorePatterns: [
+      "**/dist/**",
+      "**/.output/**",
+      "**/node_modules/**",
+      "**/components/ui/**",
+      "**/_generated/*",
+    ],
     options: {
       typeAware: true,
       typeCheck: true,
     },
   },
-})
+  fmt: {
+    ignorePatterns: [
+      "**/dist/**",
+      "**/.output/**",
+      "**/node_modules/**",
+      "**/components/ui/**",
+      "**/_generated/*"
+    ],
+    experimentalTailwindcss: {
+      stylesheet: "./layers/ui/assets/css/main.css",
+      attributes: ["class", "className"],
+      functions: ["clsx", "cn"],
+      preserveWhitespace: true,
+    },
+    bracketSpacing: true,
+    experimentalSortImports: {
+      groups: [
+        ["side-effect"],
+        ["builtin"],
+        ["external", "external-type"],
+        ["internal", "internal-type"],
+        ["parent", "parent-type"],
+        ["sibling", "sibling-type"],
+        ["index", "index-type"],
+      ],
+    },
+  },
+});
